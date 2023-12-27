4d6f0f3c97ff17eac35b80aa012c5aa5688265b1
p-malecki <p.malecki2002@gmail.com>
2023-12-27 16:01:03 +0100

    change to dotnet 8, add appsettings file, datacontext for EntityFramework

--- app/LicenseHubApp/Program.cs
+++ app/LicenseHubApp/Program.cs
@@ -1,17 +1,27 @@
+using Microsoft.Extensions.Configuration;
+
 namespace LicenseHubApp
 {
     internal static class Program
     {
+        public static IConfiguration Configuration;
+
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
+        /// 
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+            //To register all default providers:
+            var builder = new ConfigurationBuilder()
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            Configuration = builder.Build();
+
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+
         }
     }
 }
---
+++ app/LicenseHubApp/Repositories/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using LicenseHubApp.Models;
+
+
+namespace LicenseHubApp.Repositories
+{
+    internal class DataContext : DbContext
+    {
+        public DbSet<UserModel> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json", true, true);
+
+            var config = builder.Build();
+
+            optionsBuilder
+                .UseSqlite(config["ConnectionString"]);
+        }
+    }
+}
