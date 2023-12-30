7875518bb30deacfdf8ba03e1ce11668385a3975
p-malecki <p.malecki2002@gmail.com>
2023-12-30 01:21:31 +0100

    Configure database in startup obj

--- app/LicenseHubApp/Program.cs
+++ app/LicenseHubApp/Program.cs
@@ -1,3 +1,6 @@
+using LicenseHubApp.Models;
+using LicenseHubApp.Repositories;
+using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.Configuration;
 
 namespace LicenseHubApp
@@ -14,13 +17,44 @@ namespace LicenseHubApp
         [STAThread]
         static void Main(string[] args)
         {
-            //To register all default providers:
-            var builder = new ConfigurationBuilder()
-               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-            Configuration = builder.Build();
+            // create connectionString with absolute path to [project]\Data\database.db
+            var connectionString = "";
+            var solutionDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            var path = Path.Combine(solutionDirectory, "Data\\database.db");
+
+            var builder = new SqliteConnectionStringBuilder(connectionString);
+            builder.DataSource = Path.GetFullPath(
+                Path.Combine(path, builder.DataSource));
+            connectionString = builder.ToString();
+
+            System.Configuration.ConfigurationManager.AppSettings["ConnectionString"] = connectionString;
+
 
             ApplicationConfiguration.Initialize();
             //Application.Run(new Form1());
+
+
+
+
+            var dataContext = new DataContext();
+            if (!dataContext.IsDatabaseConnected())
+                throw new Exception("Database is not connected.");
+
+
+            IUserRepository repository = new UserRepository(dataContext);
+            var user = new UserModel() { Id = 123, Name = "abcd", Password = "Password1#", IsAdmin = false };
+
+
+            repository.Add(user);
+            var all = repository.GetAll().Result.ToList();
+            foreach (var u in all)
+                Console.WriteLine(u.Id);
+
+
+            repository.Delete(user);
+            all = repository.GetAll().Result.ToList();
+            foreach (var u in all)
+                Console.WriteLine(u.Id);
 
         }
     }
