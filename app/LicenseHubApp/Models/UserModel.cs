30c6c2d0d4cf54b27b255316bc41df97d8dce828
p-malecki <p.malecki2002@gmail.com>
2023-12-30 01:22:15 +0100

    Remove unused imports

--- app/LicenseHubApp/Models/UserModel.cs
+++ app/LicenseHubApp/Models/UserModel.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
-using System.Security.Cryptography;
 
 
 namespace LicenseHubApp.Models
