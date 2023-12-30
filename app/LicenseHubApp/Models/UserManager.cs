834cd41df691220d962ce2da867b9f129149781a
p-malecki <p.malecki2002@gmail.com>
2023-12-30 01:24:19 +0100

    Implement new functions in UserManager

--- app/LicenseHubApp/Models/UserManager.cs
+++ app/LicenseHubApp/Models/UserManager.cs
@@ -1,41 +1,108 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LicenseHubApp.Models
+﻿namespace LicenseHubApp.Models
 {
-    internal sealed class UserManager
+    public sealed class UserManager
     {
-        private UserManager _instance;
-        private IEnumerable<UserModel> _users;
-        private static UserManager() { }
+        private static UserManager _instance;
+        private static IUserRepository _repository;
+        private IEnumerable<UserModel> _userList;
+        private UserManager() { }
 
-        public static UserManager GetInstance()
+        public static UserManager GetInstance(IUserRepository repository)
         {
             if (_instance == null)
             {
                 _instance = new UserManager();
+                _repository = repository;
             }
             return _instance;
         }
 
-        private bool IsPasswordValid(string password)
+        public void LoadAllUsers()
         {
-            throw new NotImplementedException();
+            _userList = _repository.GetAll().Result.ToList();
         }
-        public void CreateUser()
+        public IEnumerable<UserModel> GetAllUsers()
         {
-            throw new NotImplementedException();
+            return _userList;
+        }
+
+        public void AddUser(int id, string name, string password, bool isAdmin)
+        {
+            try
+            {
+                var user = new UserModel
+                {
+                    Id = id,
+                    Name = name,
+                    Password = password,
+                    IsAdmin = isAdmin
+                };
+
+                if (!_repository.IsIdUnique(user.Id))
+                    throw new InvalidOperationException($"User with ID {user.Id} already exists.");
+                SaveUser(user);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+        public void SaveUser(UserModel user)
+        {
+            try
+            {
+                if (user.Validate())
+                {
+                    if (!_repository.IsIdUnique(user.Id))
+                        _ = _repository.Add(user);
+                    else
+                        _ = _repository.Edit(user, user.Name, user.Password, user.IsAdmin);
+                    LoadAllUsers();
+                }
+                else
+                {
+                    throw new InvalidOperationException("Model validation failed.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.Message}");
+                throw;
+            }
         }
         public void DeleteUser(UserModel user)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (!_repository.IsIdUnique(user.Id))
+                {
+                    var a = _repository.Delete(user);
+
+                }
+                else
+                {
+                    throw new InvalidOperationException($"User with ID {user.Id} does not exist.");
+                }
+                LoadAllUsers();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.Message}");
+                throw;
+            }
         }
+
+        public void ChangeUserPassword(UserModel user, string newPassword)
+        {
+            user.Password = newPassword;
+            SaveUser(user);
+        }
+
         public void PromoteUserToAdmin(UserModel user)
         {
-            throw new NotImplementedException();
+            user.IsAdmin = true;
+            SaveUser(user);
         }
 
     }
