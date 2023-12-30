36327f0b829daa20164054e9ca1e44ab135ee46f
p-malecki <p.malecki2002@gmail.com>
2023-12-30 01:17:17 +0100

    Configure sqlite database and orm

--- app/LicenseHubApp/Repositories/DataContext.cs
+++ app/LicenseHubApp/Repositories/DataContext.cs
@@ -5,26 +5,87 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Configuration;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 
 using LicenseHubApp.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.IO;
 
 
 namespace LicenseHubApp.Repositories
 {
-    public class DataContext : DbContext
+    public class DataContext: DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite(System.Configuration.ConfigurationManager.AppSettings["ConnectionString"]);
+        }
+
         public virtual DbSet<UserModel> Users { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public bool IsDatabaseConnected()
         {
-            var builder = new ConfigurationBuilder()
-                .AddJsonFile($"appsettings.json", true, true);
+            try
+            {
+                Database.OpenConnection();
+                Database.CloseConnection();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
 
-            var config = builder.Build();
+        public IEnumerable<string> GetTables()
+        {
+            var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
 
-            optionsBuilder
-                .UseSqlite(config["ConnectionString"]);
+            if (databaseCreator != null && databaseCreator.Exists())
+            {
+                var connection = Database.GetDbConnection();
+                connection.Open();
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT name FROM sqlite_master WHERE type='table';";
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        var tables = new List<string>();
+
+                        while (reader.Read())
+                        {
+                            tables.Add(reader.GetString(0));
+                        }
+
+                        return tables;
+                    }
+                }
+            }
+
+            return Enumerable.Empty<string>();
+        }
+
+        public void ShowTables()
+        {
+            try
+            {
+                Database.OpenConnection();
+                var tables = GetTables();
+                foreach (var table in tables)
+                    Console.WriteLine(table);
+                Database.CloseConnection();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
+
+// make sure that when debugging in bin/Debug/../ must exist Data folder
